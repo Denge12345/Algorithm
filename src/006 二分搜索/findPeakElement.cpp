9319b6cabb3f1624ede122f6d5b365b4fe22bd9b
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-07-23 15:11:04 +0800

    Update findPeakElement.cpp

--- src/006 二分搜索/findPeakElement.cpp
+++ src/006 二分搜索/findPeakElement.cpp
@@ -41,38 +41,38 @@ public:
     }
 };
 
-//以下代码为自测使用,提交请使用以上代码
-
-const int N = 1005;
-int a[N];
-
-void solve(){
-    int n; cin >> n;
-    for(int i = 1; i <= n; i++) cin >> a[i];
-    if(a[1] > a[2]){
-        cout << 1 << endl;
-    }else if(a[n] > a[n - 1]){
-        cout << n << endl;
-    }else{
-        int l = 2, r = n - 2;
-        while(l <= r){
-            int m = (l + r) / 2;
-            if(a[m - 1] > a[m]){
-                r = m - 1;
-            }else if(a[m + 1] > a[m]){
-                l = m + 1;
-            }else if(a[m] > a[m - 1] && a[m] > a[m + 1]){
-                cout << m << endl;
-                break;
+/*
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        if(nums[0] > nums[0]){
+            return 0;
+        }else if(nums[n - 1] > nums[n - 2]){
+            return n - 1;
+        }else{
+            int l = 1, r = n - 1, ans = -1;
+            while(l <= r){
+                int m = (l + r) / 2;
+                if(nums[m - 1] > nums[m]){
+                    r = m - 1;
+                }else if(nums[m + 1] > nums[m]){
+                    l = m + 1;
+                }else if(nums[m] > nums[m - 1] && nums[m] > nums[m + 1]){
+                    ans = m;
+                    break;
+                }
             }
+            return ans;
         }
     }
-    return;
-}
+};
+*/
 
 int main(){
-    int t = 1;
-    while(t--){
-        solve();
-    }
+    vector<int> nums1 = {1, 2, 3, 1};
+    vector<int> nums2 = {1, 2, 1, 3, 5, 6, 4};
+    Solution solution;
+    cout << solution.findPeakElement(nums1) << endl;
+    cout << solution.findPeakElement(nums2) << endl;
 }
