68b55ecf58638dc14e575a952abbeed3741f28dc
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-07-23 15:02:08 +0800

    Update partitionList.cpp

--- src/012 划分链表/partitionList.cpp
+++ src/012 划分链表/partitionList.cpp
@@ -17,81 +17,89 @@ class ListNode{
 };
 
 //划分链表
-//直接提交这个函数(或其中的内容)即可
-ListNode* partition(ListNode* head, int x){
-    ListNode* leftHead = nullptr; // < x 的区域
-	ListNode* leftTail = nullptr; 
-    ListNode* rightHead = nullptr; // >= x 的区域
-	ListNode* rightTail = nullptr; 
-    ListNode* next = nullptr;
-    while(head != nullptr){
-        next = head->next;
-        head->next = nullptr;
-        if(head->val < x){
-            if(leftHead == nullptr){
-                leftHead = head;
-            }else{
-                leftTail->next = head;
-            }
-            leftTail = head;
-        }else{
-            if(rightHead == nullptr){
-                rightHead = head;
-            }else{
-                rightTail->next = head;
-            }
-            rightTail = head;
-        }
-        head = next;
-    }
-    if(leftHead == nullptr){
-        return rightHead;
-    }
-    // < x 的区域有内容
-    leftTail->next = rightHead;
-    return leftHead;
-}
+//直接提交如下类即可
+class Solution {
+public:
+	ListNode* partition(ListNode* head, int x){
+	    ListNode* leftHead = nullptr; // < x 的区域
+		ListNode* leftTail = nullptr; 
+	    ListNode* rightHead = nullptr; // >= x 的区域
+		ListNode* rightTail = nullptr; 
+	    ListNode* next = nullptr;
+	    while(head != nullptr){
+	        next = head->next;
+	        head->next = nullptr;
+	        if(head->val < x){
+	            if(leftHead == nullptr){
+	                leftHead = head;
+	            }else{
+	                leftTail->next = head;
+	            }
+	            leftTail = head;
+	        }else{
+	            if(rightHead == nullptr){
+	                rightHead = head;
+	            }else{
+	                rightTail->next = head;
+	            }
+	            rightTail = head;
+	        }
+	        head = next;
+	    }
+	    if(leftHead == nullptr){
+	        return rightHead;
+	    }
+	    // < x 的区域有内容
+	    leftTail->next = rightHead;
+	    return leftHead;
+	}
+};
 
 //划分链表
 //创建新链表的方法,比较费空间
-//直接提交这个函数(或其中的内容)即可
-/*ListNode* partition(ListNode* head, int x) {
-    if(head == nullptr){
-        return head;
-    }
-    ListNode* ans1 = nullptr; // < x 的区域
-    ListNode* ans2 = nullptr; // >= x 的区域
-    ListNode* cur1 = nullptr; // < x 区域,当前的指针
-    ListNode* cur2 = nullptr; // >= x 的区域,当前的指针
-    int val;
-    while(head != nullptr){
-        val = head->val;
-        if(val < x){
-            if(ans1 == nullptr){
-                ans1 = new ListNode(val, nullptr);
-                cur1 = ans1;
-            }else{
-                cur1->next = new ListNode(val, nullptr);
-                cur1 = cur1->next;
-            }
-        }else{
-            if(ans2 == nullptr){
-                ans2 = new ListNode(val, nullptr);
-                cur2 = ans2;
-            }else{
-                cur2->next = new ListNode(val, nullptr);
-                cur2 = cur2->next;
-            }
-        }
-        head = head->next;
-    }
-    //如果都有内容就合并
-    if(ans1 != nullptr && ans2 != nullptr){
-        cur1->next = ans2;
-    }
-    //只返回有内容的区域
-    return ans1 != nullptr ? ans1 : ans2;
-}*/
+//直接提交如下类即可
+/*
+class Solution {
+public:
+	ListNode* partition(ListNode* head, int x) {
+	    if(head == nullptr){
+	        return head;
+	    }
+	    ListNode* ans1 = nullptr; // < x 的区域
+	    ListNode* ans2 = nullptr; // >= x 的区域
+	    ListNode* cur1 = nullptr; // < x 区域,当前的指针
+	    ListNode* cur2 = nullptr; // >= x 的区域,当前的指针
+	    int val;
+	    while(head != nullptr){
+	        val = head->val;
+	        if(val < x){
+	            if(ans1 == nullptr){
+	                ans1 = new ListNode(val, nullptr);
+	                cur1 = ans1;
+	            }else{
+	                cur1->next = new ListNode(val, nullptr);
+	                cur1 = cur1->next;
+	            }
+	        }else{
+	            if(ans2 == nullptr){
+	                ans2 = new ListNode(val, nullptr);
+	                cur2 = ans2;
+	            }else{
+	                cur2->next = new ListNode(val, nullptr);
+	                cur2 = cur2->next;
+	            }
+	        }
+	        head = head->next;
+	    }
+	    //如果都有内容就合并
+	    if(ans1 != nullptr && ans2 != nullptr){
+	        cur1->next = ans2;
+	    }
+	    //只返回有内容的区域
+	    return ans1 != nullptr ? ans1 : ans2;
+	}
+};
+*/
 
 //输出单链表
 void printList(ListNode* head) {
@@ -112,9 +120,10 @@ void testPartitionList(){
     ListNode* Node1 = new ListNode(1, Node2);
     ListNode* Head = new ListNode(-1, Node1);
     cout << "原链表: ";
+	Solution solution;
     printList(Head);
     cout << "划分后: ";
-    printList(partition(Head, 3));
+    printList(solution.partition(Head, 3));
 }
 
 int main(){
