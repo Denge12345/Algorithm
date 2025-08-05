e1c23914c0ca756d02f387dd89860098cefb52e1
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-05 18:19:05 +0800

    Update Code01_SwapExclusiveOr.cpp

--- src/030 异或运算的骚操作/Code01_SwapExclusiveOr.cpp
+++ src/030 异或运算的骚操作/Code01_SwapExclusiveOr.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+//当i==j时,会出错
+void swapele(int arr[], int i, int j){
+    arr[i] = arr[i] ^ arr[j];
+    arr[j] = arr[i] ^ arr[j];
+    arr[i] = arr[i] ^ arr[j];
+}
+
+int main(){
+    int a = -2323;
+    int b = 10;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    cout << a << endl;
+    cout << b << endl;
+    int arr[2] = {3, 5};
+    swapele(arr, 0, 1);
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    swapele(arr, 0, 0);
+    cout << arr[0] << endl;
+    return 0;
+}
