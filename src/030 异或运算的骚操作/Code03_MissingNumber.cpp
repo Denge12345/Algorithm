1b7fcc461cf0ac5ba2a852bcd529a4aad6286802
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-06 15:32:58 +0800

    Update Code03_MissingNumber.cpp

--- src/030 异或运算的骚操作/Code03_MissingNumber.cpp
+++ src/030 异或运算的骚操作/Code03_MissingNumber.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 找到缺失的数字
+// 测试链接 : https://leetcode.cn/problems/missing-number/
+
+int missingNumber(vector<int>& nums) {
+    int eorAll = 0, eorHas = 0, n = nums.size();
+    for(int i = 0; i < n; i++){
+        eorAll ^= i;
+        eorHas ^= nums[i];
+    }
+    eorAll ^= n;
+    return eorAll ^ eorHas;
+}
+
+int main(){
+    vector<int> nums1 = {3, 0, 1};
+    vector<int> nums2 = {0, 1};
+    vector<int> nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+    cout << missingNumber(nums1) << endl;
+    cout << missingNumber(nums2) << endl;
+    cout << missingNumber(nums3) << endl;
+    return 0;
+}
