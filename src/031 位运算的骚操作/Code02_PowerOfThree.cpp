9cd4fce0854f433bbbdf0400856528726b831455
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-08 21:31:10 +0800

    Update Code02_PowerOfThree.cpp

--- src/031 位运算的骚操作/Code02_PowerOfThree.cpp
+++ src/031 位运算的骚操作/Code02_PowerOfThree.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 判断一个整数是不是3的幂
+// 测试链接 : https://leetcode.cn/problems/power-of-three/
+
+bool isPowerOfThree(int n) {
+    return n > 0 && 1162261467 % n == 0;
+}
+
+int main(){
+    int n1 = 27, n2 = 0, n3 = 9, n4 = 45;
+    cout << isPowerOfThree(n1) << endl;
+    cout << isPowerOfThree(n2) << endl;
+    cout << isPowerOfThree(n3) << endl;
+    cout << isPowerOfThree(n4) << endl;
+    return 0; 
+}
