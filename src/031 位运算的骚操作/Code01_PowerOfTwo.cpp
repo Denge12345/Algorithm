41c53f0ef9decbae4e60b96078ae3d658207583a
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-08 17:26:57 +0800

    Update Code01_PowerOfTwo.cpp

--- src/031 位运算的骚操作/Code01_PowerOfTwo.cpp
+++ src/031 位运算的骚操作/Code01_PowerOfTwo.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// Brian Kernighan算法
+// 提取出二进制里最右侧的1
+// 判断一个整数是不是2的幂
+// 测试链接 : https://leetcode.cn/problems/power-of-two/
+
+bool isPowerOfTwo(int n) {
+    return n > 0 && n == (n & -n);
+}
+
+int main(){
+    int n1 = 1, n2 = 16, n3 = 3;
+    cout << isPowerOfTwo(n1) << endl;
+    cout << isPowerOfTwo(n2) << endl;
+    cout << isPowerOfTwo(n3) << endl;
+    return 0;
+}
