68ee42e08fa9e16dd23f2063dc2c5b6422bf4fd6
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-19 14:00:40 +0800

    Update Code03_MissingNumber.cpp

--- src/030 异或运算的骚操作/Code03_MissingNumber.cpp
+++ src/030 异或运算的骚操作/Code03_MissingNumber.cpp
@@ -3,23 +3,27 @@ using namespace std;
 
 // 找到缺失的数字
 // 测试链接 : https://leetcode.cn/problems/missing-number/
-
-int missingNumber(vector<int>& nums) {
-    int eorAll = 0, eorHas = 0, n = nums.size();
-    for(int i = 0; i < n; i++){
-        eorAll ^= i;
-        eorHas ^= nums[i];
+// 直接提交如下类即可
+class Solution{
+public:
+    int missingNumber(vector<int>& nums) {
+        int eorAll = 0, eorHas = 0, n = nums.size();
+        for(int i = 0; i < n; i++){
+            eorAll ^= i;
+            eorHas ^= nums[i];
+        }
+        eorAll ^= n;
+        return eorAll ^ eorHas;
     }
-    eorAll ^= n;
-    return eorAll ^ eorHas;
-}
+};
 
 int main(){
+    Solution solution;
     vector<int> nums1 = {3, 0, 1};
     vector<int> nums2 = {0, 1};
     vector<int> nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
-    cout << missingNumber(nums1) << endl;
-    cout << missingNumber(nums2) << endl;
-    cout << missingNumber(nums3) << endl;
+    cout << solution.missingNumber(nums1) << endl;
+    cout << solution.missingNumber(nums2) << endl;
+    cout << solution.missingNumber(nums3) << endl;
     return 0;
 }
