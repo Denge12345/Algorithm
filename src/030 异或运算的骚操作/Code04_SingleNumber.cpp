101f674a9264b934378709b9b4b8147424eb3c4b
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-19 14:01:30 +0800

    Update Code04_SingleNumber.cpp

--- src/030 异或运算的骚操作/Code04_SingleNumber.cpp
+++ src/030 异或运算的骚操作/Code04_SingleNumber.cpp
@@ -4,21 +4,25 @@ using namespace std;
 // 数组中1种数出现了奇数次，其他的数都出现了偶数次
 // 返回出现了奇数次的数
 // 测试链接 : https://leetcode.cn/problems/single-number/
-
-int singleNumber(vector<int>& nums) {
-    int eor = 0;
-    for(int num : nums){
-        eor ^= num;
+// 直接提交如下类即可
+class Solution{
+public:
+    int singleNumber(vector<int>& nums) {
+        int eor = 0;
+        for(int num : nums){
+            eor ^= num;
+        }
+        return eor;
     }
-    return eor;
-}
+};
 
 int main(){
+    Solution solution;
     vector<int> nums1 = {2, 2, 1};
     vector<int> nums2 = {4, 1, 2, 1, 2};
     vector<int> nums3 = {1};
-    cout << singleNumber(nums1) << endl;
-    cout << singleNumber(nums2) << endl;
-    cout << singleNumber(nums3) << endl;
+    cout << solution.singleNumber(nums1) << endl;
+    cout << solution.singleNumber(nums2) << endl;
+    cout << solution.singleNumber(nums3) << endl;
     return 0;
 }
