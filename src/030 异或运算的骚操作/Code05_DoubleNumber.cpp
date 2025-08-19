d171e953e174c7cd80576db53a3ff2a94bbc48cf
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-19 14:03:38 +0800

    Update Code05_DoubleNumber.cpp

--- src/030 异或运算的骚操作/Code05_DoubleNumber.cpp
+++ src/030 异或运算的骚操作/Code05_DoubleNumber.cpp
@@ -4,31 +4,35 @@ using namespace std;
 // 数组中有2种数出现了奇数次，其他的数都出现了偶数次
 // 返回这2种出现了奇数次的数
 // 测试链接 : https://leetcode.cn/problems/single-number-iii/
-
-vector<int> singleNumber(vector<int>& nums) {
-    long long eor1 = 0;
-    for(int num : nums){
-        eor1 ^= num;
-    }
-    long long rightOne = eor1 & (-eor1);
-    long long eor2 = 0;
-    for(int num : nums){
-        if((num & rightOne) == 0){
-            eor2 ^= num;
+// 直接提交如下类即可
+class Solution{
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        long long eor1 = 0;
+        for(int num : nums){
+            eor1 ^= num;
         }
+        long long rightOne = eor1 & (-eor1);
+        long long eor2 = 0;
+        for(int num : nums){
+            if((num & rightOne) == 0){
+                eor2 ^= num;
+            }
+        }
+        return {(int)eor2, (int)(eor1 ^ eor2)};
     }
-    return {(int)eor2, (int)(eor1 ^ eor2)};
-}
+};
 
 int main(){
+    Solution solution;
     vector<int> nums1 = {1, 2, 1, 3, 2, 5};
     vector<int> nums2 = {-1, 0};
     vector<int> nums3 = {0, 1};
-    for(int num : singleNumber(nums1)) cout << num << " ";
+    for(int num : solution.singleNumber(nums1)) cout << num << " ";
     cout << endl;
-    for(int num : singleNumber(nums2)) cout << num << " ";
+    for(int num : solution.singleNumber(nums2)) cout << num << " ";
     cout << endl;
-    for(int num : singleNumber(nums3)) cout << num << " ";
+    for(int num : solution.singleNumber(nums3)) cout << num << " ";
     cout << endl;
     return 0;
 }
