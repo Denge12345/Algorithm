23f7bedd8ffcfe3dfda753e87d619d4a9a94ba1a
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-23 21:19:47 +0800

    Update Code05_PreorderSerializeAndDeserialize.cpp

--- src/035 二叉树高频题目上/Code05_PreorderSerializeAndDeserialize.cpp
+++ src/035 二叉树高频题目上/Code05_PreorderSerializeAndDeserialize.cpp
@@ -1,1 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 二叉树先序序列化和反序列化
+// 测试链接 : https://leetcode.cn/problems/serialize-and-deserialize-binary-tree/
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string serializeString = "";
+        f(root, serializeString);
+        return serializeString;
+    }
+    void f(TreeNode* root, string & s){
+        if(root == nullptr){
+            s += "None,";
+        }else{
+            s += to_string(root->val) + ",";
+            f(root->left, s);
+            f(root->right, s);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    int cnt; //数组进行到了哪一个位置
+    TreeNode* deserialize(string data) {
+        cnt = 0;
+        vector<string> dataArray;
+        string str;
+        for(auto& ch : data){
+            if(ch == ','){
+                dataArray.push_back(str);
+                str.clear();
+            }else{
+                str.push_back(ch);
+            }
+        }
+        //如果最后一个还有内容
+        if(!str.empty()){
+            dataArray.push_back(str);
+            str.clear();
+        }
+        return g(dataArray);
+    }
+    TreeNode* g(vector<string>& dataArray){
+        string cur = dataArray[cnt++];
+        if(cur == "None"){
+            return nullptr;
+        }else{
+            TreeNode* head = new TreeNode(stoi(cur));
+            head->left = g(dataArray);
+            head->right = g(dataArray);
+            return head;
+        }
+    }
+};
+
+void test01(){
+    cout << "=====测试1=====" << endl;
+    TreeNode* Node5 = new TreeNode(5);
+    TreeNode* Node4 = new TreeNode(4);
+    TreeNode* Node3 = new TreeNode(3);
+    TreeNode* Node2 = new TreeNode(2);
+    TreeNode* Node1 = new TreeNode(1);
+    Node1->left = Node2, Node1->right = Node3;
+    Node3->left = Node4, Node3->right = Node5;
+    Codec solution;
+    cout << solution.serialize(Node1) << endl;
+}
+
+void test02(){
+    cout << "=====测试2=====" << endl;
+    Codec solution;
+    cout << solution.serialize(nullptr) << endl;
+}
+
+void test03(){
+    cout << "=====测试3=====" << endl;
+    TreeNode* Node1 = new TreeNode(1);
+    Codec solution;
+    cout << solution.serialize(Node1) << endl;
+}
+
+void test04(){
+    cout << "=====测试4=====" << endl;
+    TreeNode* Node2 = new TreeNode(2);
+    TreeNode* Node1 = new TreeNode(1);
+    Node1->left = Node2;
+    Codec solution;
+    cout << solution.serialize(Node1) << endl;
+}
+
+int main(){
+    test01();
+    test02();
+    test03();
+    test04();
+    return 0;
+}
