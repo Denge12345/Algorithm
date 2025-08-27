fa98d160ebe23223d82cfe9a94f097e67f617469
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-27 22:37:44 +0800

    Update Code01_PowerOfTwo.cpp

--- src/031 位运算的骚操作/Code01_PowerOfTwo.cpp
+++ src/031 位运算的骚操作/Code01_PowerOfTwo.cpp
@@ -6,14 +6,18 @@ using namespace std;
 // 判断一个整数是不是2的幂
 // 测试链接 : https://leetcode.cn/problems/power-of-two/
 
-bool isPowerOfTwo(int n) {
-    return n > 0 && n == (n & -n);
+class Solution{
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && n == (n & -n);
+    }
 }
 
 int main(){
     int n1 = 1, n2 = 16, n3 = 3;
-    cout << isPowerOfTwo(n1) << endl;
-    cout << isPowerOfTwo(n2) << endl;
-    cout << isPowerOfTwo(n3) << endl;
+    Solution solution;
+    cout << solution.isPowerOfTwo(n1) << endl;
+    cout << solution.isPowerOfTwo(n2) << endl;
+    cout << solution.isPowerOfTwo(n3) << endl;
     return 0;
 }
