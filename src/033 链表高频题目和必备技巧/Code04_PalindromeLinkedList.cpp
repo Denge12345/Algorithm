f95f76765360115dd6f22b410d9525dec1bac8a1
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-29 20:57:03 +0800

    Update Code04_PalindromeLinkedList.cpp

--- src/033 链表高频题目和必备技巧/Code04_PalindromeLinkedList.cpp
+++ src/033 链表高频题目和必备技巧/Code04_PalindromeLinkedList.cpp
@@ -23,47 +23,50 @@ void printList(ListNode* head) {
     cout << "NULL" << endl;
 }
 
-bool isPalindrome(ListNode* head) {
-    if(head == nullptr || head->next == nullptr){
-        return true;
-    }
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while(fast->next != nullptr && fast->next->next != nullptr){
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    ListNode* pre = slow;
-    ListNode* cur = pre->next;
-    ListNode* next = nullptr;
-    pre->next = nullptr;
-    while(cur != nullptr){
-        next = cur->next;
-        cur->next = pre;
-        pre = cur;
-        cur = next;
-    }
-    bool ans = true;
-    ListNode* left = head;
-    ListNode* right = pre;
-    while(left != nullptr && right != nullptr){
-        if(left->val != right->val){
-            ans = false;
-            break;
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == nullptr || head->next == nullptr){
+            return true;
         }
-        left = left->next;
-        right = right->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* pre = slow;
+        ListNode* cur = pre->next;
+        ListNode* next = nullptr;
+        pre->next = nullptr;
+        while(cur != nullptr){
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        bool ans = true;
+        ListNode* left = head;
+        ListNode* right = pre;
+        while(left != nullptr && right != nullptr){
+            if(left->val != right->val){
+                ans = false;
+                break;
+            }
+            left = left->next;
+            right = right->next;
+        }
+        cur = pre->next;
+        pre->next = nullptr;
+        next = nullptr;
+        while(cur != nullptr){
+            next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return ans;
     }
-    cur = pre->next;
-    pre->next = nullptr;
-    next = nullptr;
-    while(cur != nullptr){
-        next = cur->next;
-        cur->next = pre;
-        pre = cur;
-        cur = next;
-    }
-    return ans;
 }
 
 void test01(){
@@ -72,18 +75,20 @@ void test01(){
     ListNode* Node3 = new ListNode(2, Node4);
     ListNode* Node2 = new ListNode(2, Node3);
     ListNode* Node1 = new ListNode(1, Node2);
+    Solution solution;
     cout << "链表: ";
     printList(Node1);
-    cout << "是否为回文链表: " << isPalindrome(Node1) << endl;
+    cout << "是否为回文链表: " << solution.isPalindrome(Node1) << endl;
 }
 
 void test02(){
     cout << "=====测试2=====" << endl;
     ListNode* Node2 = new ListNode(2, nullptr);
     ListNode* Node1 = new ListNode(1, Node2);
+    Solution solution;
     cout << "链表: ";
     printList(Node1);
-    cout << "是否为回文链表: " << isPalindrome(Node1) << endl;
+    cout << "是否为回文链表: " << solution.isPalindrome(Node1) << endl;
 }
 
 int main(){
