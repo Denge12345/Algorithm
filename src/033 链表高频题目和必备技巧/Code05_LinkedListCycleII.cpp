e4128a8b976574f73818f6ef5c7678235a925894
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-08-29 20:58:21 +0800

    Update Code05_LinkedListCycleII.cpp

--- src/033 链表高频题目和必备技巧/Code05_LinkedListCycleII.cpp
+++ src/033 链表高频题目和必备技巧/Code05_LinkedListCycleII.cpp
@@ -24,28 +24,31 @@ void printList(ListNode* head) {
     cout << "NULL" << endl;
 }
 
-ListNode *detectCycle(ListNode *head) {
-    if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
-        return nullptr;
-    }
-    //slow指针一次走一步,fast指针一次走两步
-    ListNode* slow = head->next;
-    ListNode* fast = head->next->next;
-    while(fast != slow){
-        if(fast->next == nullptr || fast->next->next == nullptr){
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr || head->next->next == nullptr){
             return nullptr;
         }
-        slow = slow->next;
-        fast = fast->next->next;
+        //slow指针一次走一步,fast指针一次走两步
+        ListNode* slow = head->next;
+        ListNode* fast = head->next->next;
+        while(fast != slow){
+            if(fast->next == nullptr || fast->next->next == nullptr){
+                return nullptr;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        //相遇后fast指针回到开头,两个指针一次走一步
+        fast = head;
+        while(fast != slow){
+            fast = fast->next;
+            slow = slow->next;
+        }
+        //再次相遇后就是结果
+        return slow;
     }
-    //相遇后fast指针回到开头,两个指针一次走一步
-    fast = head;
-    while(fast != slow){
-        fast = fast->next;
-        slow = slow->next;
-    }
-    //再次相遇后就是结果
-    return slow;
 }
 
 void test01(){
@@ -55,7 +58,8 @@ void test01(){
     ListNode* Node2 = new ListNode(2, Node3, 1);
     ListNode* Node1 = new ListNode(3, Node2, 0);
     Node4->next = Node2, Node4->pos = 3;
-    cout << "入环节点为: " << detectCycle(Node1)->pos << endl;
+    Solution solution;
+    cout << "入环节点为: " << solution.detectCycle(Node1)->pos << endl;
 }
 
 void test02(){
@@ -63,13 +67,15 @@ void test02(){
     ListNode* Node2 = new ListNode(2);
     ListNode* Node1 = new ListNode(1, Node2, 0);
     Node2->next = Node1, Node2->pos = 1;
-    cout << "入环节点为: " << detectCycle(Node1)->pos << endl;
+    Solution solution;
+    cout << "入环节点为: " << solution.detectCycle(Node1)->pos << endl;
 }
 
 void test03(){
     cout << "=====测试3=====" << endl;
     ListNode* Node1 = new ListNode(1, nullptr, 0);
-    cout << "入环节点为: " << (detectCycle(Node1) == nullptr ? -1 : detectCycle(Node1)->pos) << endl;
+    Solution solution;
+    cout << "入环节点为: " << (solution.detectCycle(Node1) == nullptr ? -1 : solution.detectCycle(Node1)->pos) << endl;
 }
 
 int main(){
