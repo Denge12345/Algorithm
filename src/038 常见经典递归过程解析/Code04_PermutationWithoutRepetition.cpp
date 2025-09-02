1505a108461e2b4afcd387dce566c4997dbcb61c
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-02 17:41:59 +0800

    Update Code04_PermutationWithoutRepetition.cpp

--- src/038 常见经典递归过程解析/Code04_PermutationWithoutRepetition.cpp
+++ src/038 常见经典递归过程解析/Code04_PermutationWithoutRepetition.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 有重复项数组的去重全排列
+// 测试链接 : https://leetcode.cn/problems/permutations-ii/
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        f(nums, 0, ans);
+        return ans;    
+    }
+    void f(vector<int>& nums, int i, vector<vector<int>>& ans){
+        if(i == nums.size()){
+            ans.push_back(nums);
+        }else{
+            set<int> st;
+            for(int j = i; j < nums.size(); j++){
+                if(!st.count(nums[j])){
+                    st.insert(nums[j]);
+                    swap(nums[i], nums[j]);
+                    f(nums, i + 1, ans);
+                    swap(nums[i], nums[j]);
+                }
+            }
+        }
+    }
+};
+
+int main(){
+
+    return 0;
+}
