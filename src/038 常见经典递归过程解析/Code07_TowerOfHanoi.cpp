23920982930b7189cb0cbb78c801b5a257777b82
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-03 23:48:31 +0800

    Update Code07_TowerOfHanoi.cpp

--- src/038 常见经典递归过程解析/Code07_TowerOfHanoi.cpp
+++ src/038 常见经典递归过程解析/Code07_TowerOfHanoi.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void f(int i, string from, string to, string other){
+    if(i == 1){
+        cout << "移动圆盘 1 从 " << from << " 到 " << to << endl;
+    }else{
+        f(i - 1, from, other, to);
+        cout << "移动圆盘 " << i << " 从 " << from << " 到 " << to << endl;
+        f(i - 1, other, to, from);
+    }
+}
+
+void hanoi(int n){
+    if(n > 0){
+        f(n, "左", "右", "中");
+    }
+}
+
+int main(){
+    int n = 3;
+    hanoi(n);
+    return 0;
+}
