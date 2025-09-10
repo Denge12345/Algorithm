f819db8d135cce78086ff7edb3d7d0831f065216
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-10 09:26:45 +0800

    Update Code02_DecodeString.cpp

--- src/039 嵌套类问题的递归解题套路/Code02_DecodeString.cpp
+++ src/039 嵌套类问题的递归解题套路/Code02_DecodeString.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 含有嵌套的字符串解码
+// 测试链接 : https://leetcode.cn/problems/decode-string/
+
+class Solution {
+public:
+    string decodeString(string s) {
+        where = 0;
+        return f(s, 0);
+    }
+    int where;
+    string f(string& s, int i){
+        int cnt = 0;
+        string ans = "";
+        while(i < s.size() && s[i] != ']'){
+            if(s[i] >= 'a' && s[i] <= 'z'){
+                ans += s[i++];
+            }else if(s[i] >= '0' && s[i] <= '9'){
+                cnt = cnt * 10 + s[i++] - '0';
+            }else{
+                ans += get(f(s, i + 1), cnt);
+                cnt = 0;
+                i = where + 1;
+            }
+        }
+        where = i;
+        return ans;
+    }
+    string get(string s, int cnt){
+        string ans = "";
+        for(int i = 0; i < cnt; i++){
+            ans += s;
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+    return 0;
+}
