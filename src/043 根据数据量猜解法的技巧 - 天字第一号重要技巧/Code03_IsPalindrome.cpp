4f8f4ee6fc497a608b6c2445d26147e23590f744
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-14 23:00:06 +0800

    Update Code03_IsPalindrome.cpp

--- src/043 根据数据量猜解法的技巧 - 天字第一号重要技巧/Code03_IsPalindrome.cpp
+++ src/043 根据数据量猜解法的技巧 - 天字第一号重要技巧/Code03_IsPalindrome.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 超级回文数中的一个小函数，本身也是一道题 : 判断一个数字是不是回文数
+// 测试链接 : https://leetcode.cn/problems/palindrome-number/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int offset = 1;
+        while(x / offset >= 10){
+            offset *= 10;
+        }
+        while(x != 0){
+            if(x / offset != x % 10){
+                return false;
+            }
+            x = (x % offset) / 10;
+            offset /= 100;
+        }
+        return true;
+    }
+};
+
+int main(){
+    Solution solution;
+    cout << solution.isPalindrome(121) << endl;
+    cout << solution.isPalindrome(-121) << endl;
+    cout << solution.isPalindrome(10) << endl;
+    return 0;
+}
