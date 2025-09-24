e780b6d344adb78166abaed7ffa5a0f74b6ef546
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-24 15:52:02 +0800

    Update Code01_PrefixSumArray.cpp

--- src/046 构建前缀信息的技巧-解决子数组相关问题/Code01_PrefixSumArray.cpp
+++ src/046 构建前缀信息的技巧-解决子数组相关问题/Code01_PrefixSumArray.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 利用前缀和快速得到区域累加和
+// 测试链接 : https://leetcode.cn/problems/range-sum-query-immutable/
+
+class NumArray {
+public:
+    int* prefix;
+    NumArray(vector<int>& nums) {
+        prefix = new int[nums.size() + 1];
+        for(int i = 1; i <= nums.size(); i++){
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+    }
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+int main(){
+
+    return 0;
+}
