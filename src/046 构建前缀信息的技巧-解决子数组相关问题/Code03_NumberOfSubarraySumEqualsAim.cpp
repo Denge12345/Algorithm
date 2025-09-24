d85c12f83a7c90377ca2fedb121a4033a65bb3c6
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-24 18:22:10 +0800

    Update Code03_NumberOfSubarraySumEqualsAim.cpp

--- src/046 构建前缀信息的技巧-解决子数组相关问题/Code03_NumberOfSubarraySumEqualsAim.cpp
+++ src/046 构建前缀信息的技巧-解决子数组相关问题/Code03_NumberOfSubarraySumEqualsAim.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 返回无序数组中累加和为给定值的子数组个数
+// 测试链接 : https://leetcode.cn/problems/subarray-sum-equals-k/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int ans = 0;
+        mp[0] = 1;
+        for(int i = 0, sum = 0; i < nums.size(); i++){
+            sum += nums[i];
+            ans += mp[sum - k];
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+    return 0;
+}
