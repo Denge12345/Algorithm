c58fa8d0193c9cd83ba3c342a38a4768bd7938b2
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-09-26 15:32:23 +0800

    Update Code04_PositivesEqualsNegtivesLongestSubarray.cpp

--- src/046 构建前缀信息的技巧-解决子数组相关问题/Code04_PositivesEqualsNegtivesLongestSubarray.cpp
+++ src/046 构建前缀信息的技巧-解决子数组相关问题/Code04_PositivesEqualsNegtivesLongestSubarray.cpp
@@ -1,1 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 返回无序数组中正数和负数个数相等的最长子数组长度
+// 给定一个无序数组arr，其中元素可正、可负、可0
+// 求arr所有子数组中正数与负数个数相等的最长子数组的长度
+// 测试链接 : https://www.nowcoder.com/practice/545544c060804eceaed0bb84fcd992fb
+
+const int N = 1e5 + 9;
+int a[N];
+
+void solve(){
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        a[i] = (num != 0 ? (num > 0 ? 1 : -1) : 0);
+    }
+    unordered_map<int, int> mp;
+    mp[0] = -1;
+    int ans = 0;
+    for(int i = 0, sum; i < n; i++){
+        sum += a[i];
+        if(mp.count(sum)){
+            ans = max(ans, i - mp[sum]);
+        }else{
+            mp[sum] = i;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main(){
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
