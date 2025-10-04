b1aa740ef90b800aefdb2443f9117bc88adfa73c
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-10-04 12:52:17 +0800

    Update Code01_PrefixSumMatrix.cpp

--- src/048 二维前缀和、二维差分、离散化技巧/Code01_PrefixSumMatrix.cpp
+++ src/048 二维前缀和、二维差分、离散化技巧/Code01_PrefixSumMatrix.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 利用二维前缀和信息迅速得到二维区域和
+// 测试链接 : https://leetcode.cn/problems/range-sum-query-2d-immutable/
+
+class NumMatrix {
+public:
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        sum.resize(n + 1, vector<int>(m + 1, 0));
+        for(int a = 1, c = 0; c < n; a++, c++){
+            for(int b = 1, d = 0; d < m; b++, d++){
+                sum[a][b] = matrix[c][d];
+            }
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+            }
+        }
+    }
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - sum[row1][col2 + 1] + sum[row1][col1];
+    }
+};
+
+int main(){
+
+    return 0;
+}
