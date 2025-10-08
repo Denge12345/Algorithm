612642b1d4e72d81d40e840eda490e5fa056e80c
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-10-08 18:19:57 +0800

    Update Code02_LongestSubstringWithoutRepeatingCharacters.cpp

--- src/049 滑动窗口技巧与相关题目/Code02_LongestSubstringWithoutRepeatingCharacters.cpp
+++ src/049 滑动窗口技巧与相关题目/Code02_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 无重复字符的最长子串
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+// 测试链接 : https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        int last[256];
+        fill(last, last + 256, -1);
+        int ans = 0;
+        for(int l = 0, r = 0; r < n; r++){
+            l = max(l, last[s[r]] + 1);
+            ans = max(ans, r - l + 1);
+            last[s[r]] = r;
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+    return 0;
+}
