f9490fb8c2a53717245d3adfb016cfb3a04051f1
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-10-08 23:31:54 +0800

    Update Code04_GasStation.cpp

--- src/049 滑动窗口技巧与相关题目/Code04_GasStation.cpp
+++ src/049 滑动窗口技巧与相关题目/Code04_GasStation.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 加油站
+// 在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
+// 你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升
+// 你从其中的一个加油站出发，开始时油箱为空。
+// 给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周
+// 则返回出发时加油站的编号，否则返回 -1
+// 如果存在解，则 保证 它是 唯一 的。
+// 测试链接 : https://leetcode.cn/problems/gas-station/
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        for(int l = 0, r = 0, sum; l < n; l = r + 1, r = l){
+            sum = 0;
+            while(sum + gas[r % n] - cost[r % n] >= 0){
+                if(r - l + 1 == n){
+                    return l;
+                }
+                sum += gas[r % n] - cost[r % n];
+                r++;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+
+    return 0;
+}
