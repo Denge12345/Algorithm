84a283314c555a12da0254fa9b92ab990d8dbec9
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-10-24 19:05:24 +0800

    Add dailyTemperatures function in Code02_DailyTemperatures.cpp
    Implement daily temperature calculation using a monotonic stack.

--- src/051 单调栈 - 上/Code02_DailyTemperatures.cpp
+++ src/051 单调栈 - 上/Code02_DailyTemperatures.cpp
@@ -1,1 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 每日温度
+// 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer
+// 其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后
+// 如果气温在这之后都不会升高，请在该位置用 0 来代替。
+// 测试链接 : https://leetcode.cn/problems/daily-temperatures/
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        int r = 0;
+        vector<int> ans(n), stk(n + 1);
+        for(int i = 0; i < n; i++){
+            while(r > 0 && temperatures[stk[r - 1]] < temperatures[i]){
+                int cur = stk[--r];
+                ans[cur] = i - cur; 
+            }
+            stk[r++] = i;
+        }
+        return ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
