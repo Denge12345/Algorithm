632c95c46b3d11415a429c19c2633232a7980dcc
Denge12345 <62041021+Denge12345@users.noreply.github.com>
2025-10-25 14:51:35 +0800

    Add solution for sum of subarray minimums
    Implement a solution to calculate the sum of minimums of all subarrays in a given integer array, using a monotonic stack approach.

--- src/051 单调栈 - 上/Code03_SumOfSubarrayMinimums.cpp
+++ src/051 单调栈 - 上/Code03_SumOfSubarrayMinimums.cpp
@@ -1,1 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+// 子数组的最小值之和
+// 给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
+// 由于答案可能很大，答案对 1000000007 取模
+// 测试链接 : https://leetcode.cn/problems/sum-of-subarray-minimums/
+
+class Solution {
+public:
+    int mod = 1000000007;
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size(), r = 0;
+        vector<int> stk(n);
+        long long ans = 0;
+        for(int i = 0; i < n; i++){
+            while(r > 0 && arr[stk[r - 1]] >= arr[i]){
+                int cur = stk[--r];
+                int left = r == 0 ? -1 : stk[r - 1];
+                ans = (ans + 1ll * (cur - left) * (i - cur) * arr[cur]) % mod;
+            }
+            stk[r++] = i;
+        }
+        while(r > 0){
+            int cur = stk[--r];
+            int left = r == 0 ? -1 : stk[r - 1];
+            ans = (ans + 1ll * (cur - left) * (n - cur) * arr[cur]) % mod;
+        }
+        return (int)ans;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
